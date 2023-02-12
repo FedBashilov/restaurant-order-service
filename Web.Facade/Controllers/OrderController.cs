1b876807f14cf616a87317088e0cda787d8e4bfa
fbashilov <fedbashilov@gmail.com>
2023-02-12 15:29:56 +0300

    Add response codes

--- Web.Facade/Controllers/OrderController.cs
+++ Web.Facade/Controllers/OrderController.cs
@@ -25,27 +25,43 @@ namespace Web.Facade.Controllers
         [Authorize(Roles = "cook, admin")]
         [HttpGet]
         [Route("")]
+        [ProducesResponseType(200, Type = typeof(List<OrderResponse>))]
+        [ProducesResponseType(500, Type = typeof(ErrorResponse))]
         public async Task<IActionResult> GetOrders(
             [FromQuery] int offset = 0,
             [FromQuery] int count = 100,
             [FromQuery] bool orderDesc = false)
         {
             this.logger.LogInformation($"Starting to get all orders...");
-            var accessToken = await this.HttpContext.GetTokenAsync("access_token");
-            var orders = await this.orderService.GetOrders(accessToken, offset, count, orderDesc);
 
-            this.logger.LogInformation($"All orders received successfully! Orders: {JsonSerializer.Serialize(orders)}. Sending the orders in response...");
-            return this.Ok(orders);
+            try
+            {
+                var accessToken = await this.HttpContext.GetTokenAsync("access_token");
+                var orders = await this.orderService.GetOrders(accessToken, offset, count, orderDesc);
+
+                this.logger.LogInformation($"All orders received successfully! Orders: {JsonSerializer.Serialize(orders)}. Sending the orders in response...");
+                return this.Ok(orders);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogWarning(ex, $"Can't get orders. Unexpected error. Sending 500 response...");
+                return this.StatusCode(500, new ErrorResponse($"Can't get orders. Unexpected error."));
+            }
         }
 
         [Authorize(Roles = "cook, admin")]
         [HttpGet]
         [Route("{id}")]
-        public async Task<IActionResult> GetOrder([FromRoute] int id)
+        [ProducesResponseType(200, Type = typeof(OrderResponse))]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500, Type = typeof(ErrorResponse))]
+        public async Task<IActionResult> GetOrder(
+            [FromRoute] int id)
         {
+            this.logger.LogInformation($"Starting to get order with id = {id} ...");
+
             try
             {
-                this.logger.LogInformation($"Starting to get order with id = {id} ...");
                 var accessToken = await this.HttpContext.GetTokenAsync("access_token");
                 var order = await this.orderService.GetOrder(id, accessToken);
 
@@ -55,27 +71,49 @@ namespace Web.Facade.Controllers
             catch (NotFoundException ex)
             {
                 this.logger.LogWarning(ex, $"Can't get order. Not found order with id = {id}. Sending 404 response...");
-                return this.NotFound($"Can't get order. Not found order with id = {id}");
+                return this.NotFound();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogWarning(ex, $"Can't get order. Unexpected error. Sending 500 response...");
+                return this.StatusCode(500, new ErrorResponse($"Can't get order. Unexpected error."));
             }
         }
 
         [Authorize(Roles = "client, cook, admin")]
         [HttpPost]
         [Route("")]
-        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderDto newOrder)
+        [ProducesResponseType(201, Type = typeof(OrderResponse))]
+        [ProducesResponseType(500, Type = typeof(ErrorResponse))]
+        public async Task<IActionResult> CreateOrder(
+            [FromBody] CreateOrderDto newOrder)
         {
             this.logger.LogInformation($"Starting to create order: {JsonSerializer.Serialize(newOrder)} ...");
-            var accessToken = await this.HttpContext.GetTokenAsync("access_token");
-            var order = await this.orderService.CreateOrder(newOrder, accessToken);
 
-            this.logger.LogInformation($"The order created successfully! order: {JsonSerializer.Serialize(order)}. Sending the order in response...");
-            return this.StatusCode(201, order);
+            try
+            {
+                var accessToken = await this.HttpContext.GetTokenAsync("access_token");
+                var order = await this.orderService.CreateOrder(newOrder, accessToken);
+
+                this.logger.LogInformation($"The order created successfully! order: {JsonSerializer.Serialize(order)}. Sending the order in response...");
+                return this.StatusCode(201, order);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogWarning(ex, $"Can't create order. Unexpected error. Sending 500 response...");
+                return this.StatusCode(500, new ErrorResponse($"Can't create order. Unexpected error."));
+            }
         }
 
         [Authorize(Roles = "cook, admin")]
         [HttpPut]
         [Route("{id}")]
-        public async Task<IActionResult> UpdateOrderStatus([FromRoute] int id, [FromBody] UpdateOrderStatusDto updateDto)
+        [ProducesResponseType(200, Type = typeof(OrderResponse))]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500, Type = typeof(ErrorResponse))]
+        public async Task<IActionResult> UpdateOrderStatus(
+            [FromRoute] int id,
+            [FromBody] UpdateOrderStatusDto updateDto)
         {
             try
             {
@@ -88,8 +126,13 @@ namespace Web.Facade.Controllers
             }
             catch (NotFoundException ex)
             {
-                this.logger.LogWarning(ex, $"Can't update order. Not found order with id = {id}. Sending 404 response...");
-                return this.NotFound($"Can't update order. Not found order with id = {id}");
+                this.logger.LogWarning(ex, $"Can't update order status. Not found order with id = {id}. Sending 404 response...");
+                return this.NotFound();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogWarning(ex, $"Can't update order status. Unexpected error. Sending 500 response...");
+                return this.StatusCode(500, new ErrorResponse($"Can't update order status. Unexpected error."));
             }
         }
     }
